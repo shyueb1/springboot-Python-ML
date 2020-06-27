f4e7964b8687d60ba2f62387eb9cd0a5d437ced1
shyueb1 <32813614+shyueb1@users.noreply.github.com>
2020-06-27 02:32:57 +0100

    begun working on ML classes

---
+++ src/main/java/com/example/demo/services/MLPredictor.py
@@ -0,0 +1,23 @@
+"""
+    -Connect to DB
+    -Get username argument
+    -Get user healthrecords
+    -Normalise data, apply NN on test sample (todays healthrecord)
+    -Return prediction
+"""
+
+import sys
+
+def connectToDB():
+    pass
+def getHealthRecord():
+    pass
+def normaliseData():
+    pass
+def getPrediction():
+    pass
+
+user = sys.argv[1]
+# db = new Database()
+# nn = new NearestNeighbour()
+
---
+++ src/main/java/com/example/demo/services/MLService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Service
+public class MLService {
+    public MLService() {
+    }
+
+    public String getPrediction(String user) throws IOException {
+        String prediction = "";
+        String s = null;
+        try {
+            //Call python script with username arg
+            Process p = Runtime.getRuntime().exec("python src/main/java/com/example/demo/services/MLPredictor.py "+user);
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            if((s = stdInput.readLine()) != null){
+                prediction = s;
+            }
+            // read any errors from the attempted command
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally{
+            return prediction;
+        }
+    }
+
+
+}
