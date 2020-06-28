ca3abe8d18786e17179e4c4f221b3cf5f86822f4
shyueb1 <32813614+shyueb1@users.noreply.github.com>
2020-06-28 01:52:53 +0100

    Connecting python ml service to db

--- src/main/java/com/example/demo/services/MLService.java
+++ src/main/java/com/example/demo/services/MLService.java
@@ -11,12 +11,17 @@ public class MLService {
     public MLService() {
     }
 
-    public String getPrediction(String user) throws IOException {
+    public static void main(String[] args) throws IOException {
+        MLService ml = new MLService();
+        ml.getPrediction("");
+    }
+
+    public String getPrediction(String user){
         String prediction = "";
         String s = null;
         try {
             //Call python script with username arg
-            Process p = Runtime.getRuntime().exec("python src/main/java/com/example/demo/services/MLPredictor.py "+user);
+            Process p = Runtime.getRuntime().exec("python src/main/python/MLPredictor.py "+user);
 
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
             BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
--- src/main/python/MLPredictor.py
+++ src/main/python/MLPredictor.py
@@ -7,6 +7,14 @@
 """
 
 import sys
+import psycopg2
+from decouple import config
+
+DB_NAME = config("DB_NAME")
+DB_HOST = config("DB_HOST")
+DB_USER = config("DB_USER")
+DB_PASS = config("DB_PASS")
+
 
 def connectToDB():
     pass
